83b29b36a759494c2c603fce3e2c3024937cdcf0
JeongAYoo <jeonga038@naver.com>
2021-10-09 17:26:22 +0900

    Initial Commit

---
+++ SeSSAC_DrinkWater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSSAC_DrinkWater
+//
+//  Created by Jade Yoo on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
