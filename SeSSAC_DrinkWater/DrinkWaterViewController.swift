c02005f60c2cdc44fa7f767de9c7f9d79235b479
JeongAYoo <jeonga038@naver.com>
2022-02-06 16:53:52 +0900

    일단 완성

--- SeSSAC_DrinkWater/DrinkWaterViewController.swift
+++ SeSSAC_DrinkWater/DrinkWaterViewController.swift
@@ -10,60 +10,81 @@ import UIKit
 class DrinkWaterViewController: UIViewController {
     @IBOutlet var totalWaterLbl: UILabel!   //총 마신양 레이블
     @IBOutlet var goalLbl: UILabel!         //목표 레이블
-    @IBOutlet var cactusImgView: UIImageView! //선인장 이미지
+    @IBOutlet var cactusImgView: UIImageView! //선인장 이미지뷰
     @IBOutlet var intakeText: UITextField!  //섭취량 텍스트
     @IBOutlet var recommendedAmountLbl: UILabel!    //권장량 레이블
     @IBOutlet var drinkWaterBtn: UIButton!  //물마시기 버튼
     
-    
     var cactusImage = UIImage(named: "1-1.png")
-    var nickname: String? = "안녕하세요"
-    var height: Double? = 160
-    var weight: Double? = 50
-    var totalWater: Int = 0
-    var recommendedAmount: Double?
-    var goal: Int?
+    var totalWater: Int = 0 {
+        didSet {
+            goal = calculateGoal()
+            UserDefaults.standard.set(totalWater, forKey: "totalWater")
+        }
+    }
+    var recommendedAmount: Double = 0
+    var goal: Int = 0 {
+        didSet {
+            goalLbl.text = "목표의 \(goal)%"
+            UserDefaults.standard.set(goal, forKey: "goal")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //내비게이션 바 타이틀
+        if UserDefaults.standard.bool(forKey: "isRegistered") {
+            setUser()
+            setupView()
+            updateView()
+            
+        } else {
+            //미등록유저 ProfileVC 전환
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUser()
+    }
+    
+    func setupView() {
         navigationItem.title = "물 마시기"
-        //뷰 배경색 변경
-        view.backgroundColor = UIColor(red: 0, green: 150/255, blue: 111/255, alpha: 1)
-        //바 버튼 색상 변경
+        view.backgroundColor = UIColor(named: "mainColor")
         navigationController?.navigationBar.tintColor = .white
-        //총 마신양, 권장량, 목표량 나타내는 레이블
-        recommendedAmount = calculateRecommendedAmount(height: height!, weight: weight!)
         
+        //총 마신양 레이블
         totalWaterLbl.numberOfLines = 3
         totalWaterLbl.font = UIFont.boldSystemFont(ofSize: 30)
         totalWaterLbl.adjustsFontSizeToFitWidth = true
-        totalWaterLbl.text = "잘하셨어요!\n오늘 마신 양은\n\(totalWater)ml"
+        totalWaterLbl.text = "물 마시기 시작!\n오늘 마신 양은\n\(totalWater)ml"
         totalWaterLbl.textColor = .white
         
+        //목표 퍼센트 레이블
         goal = calculateGoal()
-        goalLbl.text = "목표의 \(goal!)%"
+        goalLbl.text = "목표의 \(goal)%"
         goalLbl.font = UIFont.systemFont(ofSize: 15)
         goalLbl.textColor = .white
         goalLbl.adjustsFontSizeToFitWidth = true
     
-        //선인장 이미지 할당
+        //선인장 이미지
         cactusImgView.image = cactusImage
         
-        //섭취량 텍스트필드 키보드 설정
+        //섭취량TF
         intakeText.keyboardType = .numberPad
-        intakeText.placeholder = "물 섭취량"
-        intakeText.font = UIFont.systemFont(ofSize: 20)
-        intakeText.textAlignment = .center
+        intakeText.placeholder = "섭취량"
+        intakeText.font = UIFont.systemFont(ofSize: 25)
+        intakeText.adjustsFontSizeToFitWidth = true
+        intakeText.textAlignment = .right
         intakeText.textColor = .white
         intakeText.backgroundColor = .clear
+        intakeText.borderStyle = .none
         
-        //권장량 레이블
-        recommendedAmountLbl.text = "\(nickname!)님의 하루 물 권장 섭취량은 \(recommendedAmount!)L 입니다."
+        //권장량
         recommendedAmountLbl.textColor = .white
         recommendedAmountLbl.font = UIFont.systemFont(ofSize: 15)
-        
+        recommendedAmountLbl.textAlignment = .center
         
         //물마시기 버튼
         drinkWaterBtn.backgroundColor = .white
@@ -71,26 +92,67 @@ class DrinkWaterViewController: UIViewController {
         drinkWaterBtn.setTitleColor(.black, for: .normal)
     }
     
+    func setUser() {
+        let nickname = UserDefaults.standard.string(forKey: "nickname")!
+        let height = Double(UserDefaults.standard.string(forKey: "height")!)!
+        let weight = Double(UserDefaults.standard.string(forKey: "weight")!)!
+        
+        recommendedAmount = calculateRecommendedAmount(height: height, weight: weight)
+        recommendedAmountLbl.text = "\(nickname)님의 하루 물 권장 섭취량은 \(recommendedAmount)L 입니다."
+        
+        totalWater = UserDefaults.standard.integer(forKey: "totalWater")
+        goal = UserDefaults.standard.integer(forKey: "goal")
+    }
+
     func calculateRecommendedAmount (height: Double, weight: Double) -> Double {
         return (height + weight) / 100
     }
     
     func calculateGoal() -> Int {
-        return Int((Double(totalWater) / (recommendedAmount! * 1000) * 100))
+        return Int((Double(totalWater) / (recommendedAmount * 1000) * 100))
+    }
+    
+    func updateView() {
+        //이미지변경
+        if goal <= 10 {
+            cactusImgView.image = UIImage(named: "1-1.png")
+        } else if goal <= 20 {
+            cactusImgView.image = UIImage(named: "1-2.png")
+        } else if goal <= 30 {
+            cactusImgView.image = UIImage(named: "1-3.png")
+        } else if goal <= 40 {
+            cactusImgView.image = UIImage(named: "1-4.png")
+        } else if goal <= 50 {
+            cactusImgView.image = UIImage(named: "1-5.png")
+        } else if goal <= 60 {
+            cactusImgView.image = UIImage(named: "1-6.png")
+        } else if goal <= 70 {
+            cactusImgView.image = UIImage(named: "1-7.png")
+        } else if goal <= 80 {
+            cactusImgView.image = UIImage(named: "1-8.png")
+        } else {
+            cactusImgView.image = UIImage(named: "1-9.png")
+        }
+        
+        //레이블 변경
+        if goal < 100 {
+            totalWaterLbl.text = "잘하셨어요!\n오늘 마신 양은\n\(totalWater)ml"
+        } else {
+            totalWaterLbl.text = "목표를 달성했어요!🥳\n오늘 마신 양은\n\(totalWater)ml"
+            totalWaterLbl.textColor = .yellow
+        }
     }
     
     @IBAction func resetTotal(_ sender: UIBarButtonItem) {
         totalWater = 0
-        totalWaterLbl.text = "잘하셨어요!\n오늘 마신 양은\n\(totalWater)ml"
-        goal = calculateGoal()
-        goalLbl.text = "목표의 \(goal!)%"
+        
+        setupView()
     }
     
     @IBAction func drinkWaterBtn(_ sender: UIButton) {
-        totalWater = totalWater +  (Int(intakeText.text!) ?? 0)
-        totalWaterLbl.text = "잘하셨어요!\n오늘 마신 양은\n\(totalWater)ml"
-        goal = calculateGoal()
-        goalLbl.text = "목표의 \(goal!)%"
+        totalWater = totalWater + (Int(intakeText.text!) ?? 0)
+                
+        updateView()
     }
     
 
--- SeSSAC_DrinkWater/ProfileViewController.swift
+++ SeSSAC_DrinkWater/ProfileViewController.swift
@@ -6,16 +6,151 @@
 //
 
 import UIKit
+import TextFieldEffects
+import Toast
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var saveBarBtn: UIBarButtonItem!
+    @IBOutlet weak var cactusImgView: UIImageView!
+    @IBOutlet weak var nicknameTF: HoshiTextField!
+    @IBOutlet weak var heightTF: HoshiTextField!
+    @IBOutlet weak var weightTF: HoshiTextField!
+    
+    var cactusImage = UIImage(named: "1-1.png")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor(red: 41, green: 150, blue: 110, alpha: 1)
-        saveBarBtn.title = "저장"
+        setupView()
+        
+        if UserDefaults.standard.bool(forKey: "isRegistered") {
+            nicknameTF.text = UserDefaults.standard.string(forKey: "nickname")
+            heightTF.text = UserDefaults.standard.string(forKey: "height")
+            weightTF.text = UserDefaults.standard.string(forKey: "weight")
+            
+            updateImage()
+        } else {
+            self.navigationItem.setHidesBackButton(true, animated: false)
+        }
+        
     }
     
+    func setupView() {
+        
+        view.backgroundColor = UIColor(named: "profileColor")
+        saveBarBtn.title = "저장"
+        cactusImgView.image = cactusImage
+        
+        [nicknameTF, heightTF, weightTF].forEach {
+            $0?.borderStyle = .none
+            $0?.placeholderColor = .white
+            $0?.textColor = .white
+            $0?.text = ""
+            $0?.font = .systemFont(ofSize: 17)
+            
+            $0?.borderActiveColor = .white
+            $0?.borderInactiveColor = .white
+        }
+        
+        nicknameTF.placeholder = "닉네임을 설정해주세요."
+        heightTF.placeholder = "키(cm)를 설정해주세요."
+        weightTF.placeholder = "몸무게(kg)를 설정해주세요."
+    }
+    
+    func updateImage() {
+        let goal = UserDefaults.standard.integer(forKey: "goal")
+        
+        if goal <= 10 {
+            cactusImgView.image = UIImage(named: "1-1.png")
+        } else if goal <= 20 {
+            cactusImgView.image = UIImage(named: "1-2.png")
+        } else if goal <= 30 {
+            cactusImgView.image = UIImage(named: "1-3.png")
+        } else if goal <= 40 {
+            cactusImgView.image = UIImage(named: "1-4.png")
+        } else if goal <= 50 {
+            cactusImgView.image = UIImage(named: "1-5.png")
+        } else if goal <= 60 {
+            cactusImgView.image = UIImage(named: "1-6.png")
+        } else if goal <= 70 {
+            cactusImgView.image = UIImage(named: "1-7.png")
+        } else if goal <= 80 {
+            cactusImgView.image = UIImage(named: "1-8.png")
+        } else {
+            cactusImgView.image = UIImage(named: "1-9.png")
+        }
+    }
+    func nicknameTFCheck(_ tf: UITextField) -> Bool {
+        
+        if !textFieldNullCheck(tf) {
+            let testString: String = tf.text!
+            let pattern: String = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]*$"
+            
+            guard let _ = testString.range(of: pattern, options: .regularExpression) else {
+                print("문자 외 포함됨")
+                self.view.makeToast("닉네임은 5자 이하 한글/영어만 입력해주세요.")
+                tf.text = ""
+                return false
+            }
+            
+            if testString.count > 5 {
+                print("닉네임 5자 초과")
+                self.view.makeToast("닉네임은 5자 이하 한글/영어만 입력해주세요.")
+                tf.text = ""
+                return false
+            }
+            
+            return true
+        } else { return false }
+    }
+    
+    func numberTFCheck(_ tf: UITextField) -> Bool {
+        
+        if !textFieldNullCheck(tf) {
+            let testString: String = tf.text!
+            let pattern: String = "^[0-9]*$"
 
+            guard let _ = testString.range(of: pattern, options: .regularExpression) else {
+                print("숫자 외 포함됨")
+                self.view.makeToast("키와 몸무게는 숫자만 입력해주세요.")
+                tf.text = ""
+                return false
+            }
+            
+            if testString.count > 3 || Int(testString)! <= 0 {
+                print("키, 몸무게 3자리 초과")
+                self.view.makeToast("키와 몸무게를 올바르게 입력해주세요.")
+                tf.text = ""
+                return false
+            }
+            return true
+        } else { return false }
+    }
+    
+    func textFieldNullCheck(_ tf: UITextField) -> Bool {
+        if tf.text == "" {
+            self.view.makeToast("빈칸 없이 입력해주세요.")
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+    
+    @IBAction func saveBarButtonClicked(_ sender: UIBarButtonItem) {
+        
+        if nicknameTFCheck(nicknameTF) && numberTFCheck(heightTF) && numberTFCheck(weightTF) {
+            UserDefaults.standard.set(nicknameTF.text, forKey: "nickname")
+            UserDefaults.standard.set(heightTF.text, forKey: "height")
+            UserDefaults.standard.set(weightTF.text, forKey: "weight")
+
+            print("저장완료")
+            UserDefaults.standard.set(true, forKey: "isRegistered")
+            navigationController?.popViewController(animated: true)
+        }
+
+    }
+    
 }
